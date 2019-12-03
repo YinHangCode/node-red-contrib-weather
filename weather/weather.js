1c47f674e91c35ad5b32caba1f3b40a7031e38b8
songzh96 <986758232@qq.com>
2019-12-03 22:38:08 +0800

    version 0.1.0

--- weather/weather.js
+++ weather/weather.js
@@ -1,30 +1,69 @@
 const http = require("http");
+var validator = require('validator');
 
-module.exports = function(RED) {
-    function WeatherNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-			
-        var longitude = config.longitude;
-        var latitude = config.latitude;
+module.exports = function (RED) {
+	function WeatherNode(config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
 
-        node.on('input', function(msg) {
-			node.httpGet({longitude: longitude, latitude: latitude}).then(result => {
-				msg.payload = JSON.parse(result);
-				node.send(msg);
-			}).catch(function(err) {
-				that.error(err);
-			});
-        });
-    }
-	
-	WeatherNode.prototype.httpGet = function(params) {
+		var longitude = config.longitude;
+		var latitude = config.latitude;
+
+		node.on('input', function (msg) {
+			if (validator.isJSON(String(msg.payload))) {
+				let parameter;
+				parameter = JSON.parse(msg.payload);
+				if (parameter) {
+
+					latitude = String(parameter.latitude);
+					longitude = String(parameter.longitude);
+
+					node.httpGet({ longitude: longitude, latitude: latitude }).then(result => {
+						let weather = JSON.parse(result);
+						let temperature = weather.current.feelsLike.value + weather.current.feelsLike.unit;
+						let humidity = weather.current.humidity.value + weather.current.humidity.unit;
+						let weather_code = weather.current.weather;
+						let aqi = weather.forecastDaily.aqi.value[0];
+						let sunrise = weather.forecastDaily.sunRiseSet.value[0].from;
+						let sunset = weather.forecastDaily.sunRiseSet.value[0].to;
+
+						node.send([temperature, humidity, weather_code, aqi, sunrise, sunset, weather]);
+					}).catch(function (err) {
+						RED.log.error(err);
+					});
+
+				}
+				else {
+					RED.log.error("Json Obj is null");
+				}
+			}
+			else {
+				node.httpGet({ longitude: longitude, latitude: latitude }).then(result => {
+
+					let weather = JSON.parse(result);
+					let temperature = weather.current.feelsLike.value;
+					let humidity = weather.current.humidity.value;
+					let weather_code = weather.current.weather;
+					let aqi = weather.forecastDaily.aqi.value[0];
+					let sunrise = weather.forecastDaily.sunRiseSet.value[0].from;
+					let sunset = weather.forecastDaily.sunRiseSet.value[0].to;
+
+					node.send([temperature, humidity, weather_code, aqi, sunrise, sunset, weather]);
+				}).catch(function (err) {
+					that.error(err);
+				});
+			}
+
+		});
+	}
+
+	WeatherNode.prototype.httpGet = function (params) {
 		let that = this;
 		let encoding = 'utf8';
 		let options = {
 			hostname: "weatherapi.market.xiaomi.com",
 			port: 80,
-			path: '/wtr-v3/weather/all?' 
+			path: '/wtr-v3/weather/all?'
 				+ "latitude=" + params['latitude'] + "&"
 				+ "longitude=" + params['longitude'] + "&"
 				+ "appKey=" + 'weather20180131' + "&"
@@ -40,15 +79,15 @@ module.exports = function(RED) {
 		return new Promise((resolve, reject) => {
 			let req = http.request(options, (res) => {
 				res.setEncoding(encoding);
-				res.on('data', function(chunk) {
+				res.on('data', function (chunk) {
 					data += chunk;
 				});
-	  
-				res.on('end', function() {
+
+				res.on('end', function () {
 					resolve(data);
 				});
 			});
-	  
+
 			req.on('error', (e) => {
 				reject(new Error(e.message));
 			});
@@ -59,6 +98,6 @@ module.exports = function(RED) {
 			req.end();
 		});
 	}
-	
-    RED.nodes.registerType("weather", WeatherNode);
+
+	RED.nodes.registerType("weather", WeatherNode);
 }
