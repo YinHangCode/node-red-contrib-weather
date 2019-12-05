920276c1d23a8569075ff5cf0fcaed7889af3313
songzh96 <986758232@qq.com>
2019-12-05 11:25:48 +0800

    weather_code to weather_status

--- weather/weather.js
+++ weather/weather.js
@@ -12,7 +12,7 @@ module.exports = function (RED) {
 		let weather = new Object();
 		let temperature = new Object();
 		let humidity = new Object();
-		let weather_code = new Object();
+		let weather_status = new Object();
 		let aqi = new Object();
 		let sunrise = new Object();
 		let sunset = new Object();
@@ -32,12 +32,12 @@ module.exports = function (RED) {
 
 						temperature.payload = weather.payload.current.feelsLike.value + weather.payload.current.feelsLike.unit;
 						humidity.payload = weather.payload.current.humidity.value + weather.payload.current.humidity.unit;
-						weather_code.payload = weather.payload.current.weather;
+						weather_status.payload = weatherStatus(weather.payload.current.weather);
 						aqi.payload = weather.payload.forecastDaily.aqi.value[0];
 						sunrise.payload = weather.payload.forecastDaily.sunRiseSet.value[0].from;
 						sunset.payload = weather.payload.forecastDaily.sunRiseSet.value[0].to;
 
-						node.send([weather, temperature, humidity, weather_code, aqi, sunrise, sunset]);
+						node.send([weather, temperature, humidity, weather_status, aqi, sunrise, sunset]);
 					}).catch(function (err) {
 						RED.log.error(err);
 					});
@@ -53,12 +53,12 @@ module.exports = function (RED) {
 					weather.payload = JSON.parse(result);
 					temperature.payload = weather.payload.current.feelsLike.value + weather.payload.current.feelsLike.unit;
 					humidity.payload = weather.payload.current.humidity.value + weather.payload.current.humidity.unit;
-					weather_code.payload = weather.payload.current.weather;
+					weather_status.payload = weatherStatus(weather.payload.current.weather);
 					aqi.payload = weather.payload.forecastDaily.aqi.value[0];
 					sunrise.payload = weather.payload.forecastDaily.sunRiseSet.value[0].from;
 					sunset.payload = weather.payload.forecastDaily.sunRiseSet.value[0].to
 
-					node.send([weather, temperature, humidity, weather_code, aqi, sunrise, sunset]);
+					node.send([weather, temperature, humidity, weather_status, aqi, sunrise, sunset]);
 				}).catch(function (err) {
 					RED.log.error(err);
 				});
@@ -66,7 +66,133 @@ module.exports = function (RED) {
 
 		});
 	}
-
+	function weatherStatus(code)
+	{
+		let status = "";
+		// weather code to weather status
+		switch(code) {
+		case '0':
+			status = '晴'
+			break;
+		case '1':
+			status = '多云'
+			break;
+		case '2':
+			status = '阴'
+			break;
+		case '3':
+			status = '阵雨'
+			break;
+		case '4':
+			status = '雷阵雨'
+			break;
+		case '5':
+			status = '雷阵雨并伴有冰雹'
+			break;
+		case '6':
+			status = '雨夹雪'
+			break;
+		case '7':
+			status = '小雨'
+			break;
+		case '8':
+			status = '中雨'
+			break;
+		case '9':
+			status = '大雨'
+			break;
+		case '10':
+			status = '暴雨'
+			break;
+		case '11':
+			status = '大暴雨'
+			break;
+		case '12':
+			status = '特大暴雨'
+			break;
+		case '13':
+			status = '阵雪'
+			break;
+		case '14':
+			status = '小雪'
+			break;
+		case '15':
+			status = '中雪'
+			break;
+		case '16':
+			status = '大雪'
+			break;
+		case '17':
+			status = '暴雪'
+			break;
+		case '18':
+			status = '雾'
+			break;
+		case '19':
+			status = '冻雨'
+			break;
+		case '20':
+			status = '沙尘暴'
+			break;
+		case '21':
+			status = '小雨转中雨'
+			break;
+		case '22':
+			status = '中雨转大雨'
+			break;
+		case '23':
+			status = '大雨转暴雨'
+			break;
+		case '24':
+			status = '暴雨转大暴雨'
+			break;
+		case '25':
+			status = '大暴雨转特大暴雨'
+			break;
+		case '26':
+			status = '小雪转中雪'
+			break;
+		case '27':
+			status = '中雪转大雪'
+			break;
+		case '28':
+			status = '大雪转暴雪'
+			break;
+		case '29':
+			status = '浮沉'
+			break;
+		case '30':
+			status = '扬沙'
+			break;
+		case '31':
+			status = '强沙尘暴'
+			break;
+		case '32':
+			status = '飑'
+			break;
+		case '33':
+			status = '龙卷风'
+			break;
+		case '34':
+			status = '若高吹雪'
+			break;
+		case '35':
+			status = '轻雾'
+			break;
+		case '53':
+			status = '霾'
+			break;
+		case '99':
+			status = '未知'
+			break;
+		default:
+			status = '无数据'
+			break;
+		}
+		
+		return status;
+		
+	}
 	WeatherNode.prototype.httpGet = function (params) {
 		let that = this;
 		let encoding = 'utf8';
